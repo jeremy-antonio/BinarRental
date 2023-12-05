f7016c610afff3ec2c9f67580a93ce89da606a99
jeremy-antonio <jerz.antonio10@gmail.com>
2023-12-05 23:12:09 +0800

    React Template

---
+++ Chapter7/frontend/binar-car-rental/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const cars_api_base_url = "http://localhost:8082";
+
+export default function Login() {
+  //   const navigate = useNavigate();
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <div className="flex flex-row">
+        <div className="basis-6">Test</div>
+        <div className="basis-4">Test</div>
+      </div>
+    </div>
+  );
+}
